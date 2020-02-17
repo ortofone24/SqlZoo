66651b70ffcffe002b45902a3c4f1684b4761da9
ortofone24 <ortofone24@gmail.com>
2020-02-17 18:21:08 +0100

    formatting change

--- 03_SelectFromNobel.sql
+++ 03_SelectFromNobel.sql
@@ -70,8 +70,8 @@ SELECT winner, subject
 FROM nobel
 WHERE yr=1984
 ORDER BY 
-	CASE 
-	  WHEN subject IN ('Physics','Chemistry') THEN 1 
-	  ELSE 0 
-	END, 
+  CASE 
+    WHEN subject IN ('Physics','Chemistry') THEN 1 
+    ELSE 0 
+  END, 
 subject,winner
